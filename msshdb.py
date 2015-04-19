d3205f49c927d6173e579ac3d847d7f8d168bc43
Wayne Xue <xwsoul@gmail.com>
2015-04-19 21:36:22 +0800

    alias for connection beta

--- msshdb.py
+++ msshdb.py
@@ -13,6 +13,7 @@ import sqlite3
 class Database(object):
     """Database init"""
     def __init__(self, version_num):
+        self.upgrade_action_existed = False
         self.version_num = version_num
         home = os.path.expanduser("~") + "/.mssh"
         if not os.path.exists(home):
@@ -23,18 +24,19 @@ class Database(object):
         self.upgrade()
 
     def add_connection(self, host, user="root", passwd="", port=22,
-                       id_file=""):
+                       id_file="", alias=""):
         """add connection"""
         sql = '''INSERT INTO connections
-                 (id, host, user, password, port, idfile)
-                 VALUES(NULL, :host, :user, :passwd, :port, :idfile)'''
+                 (id, host, user, password, port, idfile, alias)
+                 VALUES(NULL, :host, :user, :passwd, :port, :idfile, :alias)'''
         self.conn.execute(sql, {"host": host, "user": user, "passwd": passwd,
-                                "port": port, "idfile": id_file})
+                                "port": port, "idfile": id_file,
+                                "alias": alias})
         self.conn.commit()
 
-    def find_connections(self, host='', user='', port=0, cid=0, ecid=0):
+    def find_connections(self, host='', user='', port=0, cid=0, alias='', ecid=0):
         """find connection via host, user and port"""
-        sql = '''SELECT id, host, user, port, password, idfile
+        sql = '''SELECT id, host, user, port, password, idfile, alias
                  FROM connections'''
         cond = ''
         if host != '':
@@ -51,11 +53,13 @@ class Database(object):
             cond += ' AND id = :id'
         if ecid != 0:
             cond += ' AND id != :eid'
+        if alias != '':
+            cond += ' AND alias = :alias'
         if cond != '':
             cond = ' WHERE 1' + cond
         sql += cond + ' ORDER BY id ASC'
         param = {"host": host, "user": user, "port": port, "id": cid,
-                 "eid": ecid}
+                 "alias": alias, "eid": ecid}
         return self.conn.execute(sql, param).fetchall()
 
     def get_connection_dict(self, r):
@@ -84,10 +88,6 @@ class Database(object):
         self.conn.execute(sql, {"id": cid, "val": value})
         self.conn.commit()
 
-    def add_tag(self, ser_name, project_name):
-        """add tag for connection"""
-        pass
-
     def init(self):
         """init system database"""
         sqls = {
@@ -111,16 +111,16 @@ class Database(object):
             "version": ('CREATE TABLE version (version_num INTEGER NOT NULL);',
                         'INSERT INTO version (version_num) VALUES (1);')
         }
-        exists_sql = ('''SELECT name FROM sqlite_master
-                         WHERE type='table' AND name=?;''')
-        r = self.conn.execute(exists_sql, ["version"]).fetchone()
-        if r is None:
+        if not self.table_exists("version"):
             for table in sqls.keys():
                 self.just_do_it(sqls[table])
 
     def upgrade(self):
         '''upgrade system database'''
         upgrades = {
+            3: ("ALTER TABLE connections ADD alias TEXT DEFAULT '' NOT NULL;",
+                "CREATE INDEX IF NOT EXISTS alias ON connections (alias);",
+                "DROP TABLE IF EXISTS tags;")
         }
         sql = '''SELECT version_num FROM version;'''
         r = self.conn.execute(sql).fetchone()
@@ -130,12 +130,20 @@ class Database(object):
         else:
             version = int(r[0])
         if version != self.version_num:
-            for ver in upgrades:
-                if ver > version and ver <= self.version_num:
-                    self.just_do_it(upgrades[ver])
-            self.conn.execute("DELETE FROM version;")
-            self.conn.execute("INSERT INTO version(version_num) VALUES ("
-                              + str(self.version_num) + ");")
+            self.record_backup_action(version, "backup")
+            if(self.backup()):
+                print "Database upgrading:"
+                for ver in upgrades:
+                    self.record_backup_action(ver, "upgrade")
+                    if ver > version and ver <= self.version_num:
+                        print "Upgrading to version " + str(ver) + ", latest " + str(self.version_num) + "."
+                        self.just_do_it(upgrades[ver])
+                self.just_do_it(("DELETE FROM version;",
+                                 "INSERT INTO version(version_num) VALUES ("
+                                 + str(self.version_num) + ");",
+                                 "DELETE FROM upgrade_action;"))
+
+                print "Done."
 
     def just_do_it(self, sqls):
         '''execute one or more sqls and no return'''
@@ -144,3 +152,31 @@ class Database(object):
                 self.conn.execute(sql)
         else:
             self.conn.execute(sqls)
+        self.conn.commit()
+
+    def backup(self):
+        ''''backup database'''
+        print "Generating backup:",
+        r = False
+        if(True):
+            r = True
+            print "done."
+        else:
+            print "failed."
+        return r
+
+    def record_backup_action(self, ver, action):
+        '''Record backup actions'''
+        if not self.upgrade_action_existed:
+            if not self.table_exists("upgrade_action"):
+                sql = '''CREATE TABLE upgrade_action
+                         (version INTEGER NOT NULL,
+                          action TEXT NOT NULL);'''
+                self.conn.execute(sql)
+            self.upgrade_action_existed = True
+        pass
+
+    def table_exists(self, table):
+        exists_sql = ('''SELECT name FROM sqlite_master
+                         WHERE type='table' AND name=?;''')
+        return self.conn.execute(exists_sql, [table]).fetchone() is not None
